31fae18f91e14cc33768fbf37443e1800e143202
Hugo Bijaoui <91726252+I-blueHand-I@users.noreply.github.com>
2023-02-16 18:00:08 +0100

    First commit for LED examples

---
+++ artnet_test/artnet_test.ino
@@ -0,0 +1,164 @@
+/*
+This example will receive multiple universes via Art-Net and control a strip of
+WS2812 LEDs via the FastLED library: https://github.com/FastLED/FastLED
+This example may be copied under the terms of the MIT license, see the LICENSE file for details
+*/
+#include <ArtnetWifi.h>
+#include <Arduino.h>
+#include <FastLED.h>
+
+// Wifi settings
+const char* ssid = "PixelWar";
+//const char* password = "***********";
+String hostname = "ESPGOD";
+
+
+const uint8_t PROGMEM gamma8[] = {
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1,
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2,
+  2, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5,
+  5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 9, 9, 9, 10,
+  10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
+  17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
+  25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
+  37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
+  51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
+  69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
+  90, 92, 93, 95, 96, 98, 99, 101, 102, 104, 105, 107, 109, 110, 112, 114,
+  115, 117, 119, 120, 122, 124, 126, 127, 129, 131, 133, 135, 137, 138, 140, 142,
+  144, 146, 148, 150, 152, 154, 156, 158, 160, 162, 164, 167, 169, 171, 173, 175,
+  177, 180, 182, 184, 186, 189, 191, 193, 196, 198, 200, 203, 205, 208, 210, 213,
+  215, 218, 220, 223, 225, 228, 231, 233, 236, 239, 241, 244, 247, 249, 252, 255
+};
+
+// LED settings
+const int numLeds = 441;                   // CHANGE FOR YOUR SETUP
+const int numberOfChannels = numLeds * 3;  // Total number of channels you want to receive (1 led = 3 channels)
+const byte dataPin = 6;
+CRGB leds[numLeds];
+
+// Art-Net settings
+ArtnetWifi artnet;
+const int startUniverse = 0;  // CHANGE FOR YOUR SETUP most software this is 1, some software send out artnet first universe as 0.
+
+// Check if we got all universes
+const int maxUniverses = numberOfChannels / 512 + ((numberOfChannels % 512) ? 1 : 0);
+bool universesReceived[maxUniverses];
+bool sendFrame = 1;
+int previousDataLength = 0;
+
+
+// connect to wifi – returns true if successful or false if not
+boolean ConnectWifi(void) {
+  boolean state = true;
+  int i = 0;
+  WiFi.mode(WIFI_STA);
+  WiFi.config(INADDR_NONE, INADDR_NONE, INADDR_NONE, INADDR_NONE);
+  WiFi.setHostname(hostname.c_str()); //define hostname
+  WiFi.begin(ssid) //, password);
+  Serial.println("");
+  Serial.println("Connecting to WiFi");
+
+  // Wait for connection
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    if (i > 20) {
+      state = false;
+      break;
+    }
+    i++;
+  }
+  if (state) {
+    Serial.println("");
+    Serial.print("Connected to ");
+    Serial.println(ssid);
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+  } else {
+    Serial.println("");
+    Serial.println("Connection failed.");
+  }
+
+  return state;
+}
+
+void initTest() {
+  for (int i = 0; i < numLeds; i++) {
+    leds[i] = CRGB(127, 0, 0);
+  }
+  FastLED.show();
+  delay(500);
+  for (int i = 0; i < numLeds; i++) {
+    leds[i] = CRGB(0, 127, 0);
+  }
+  FastLED.show();
+  delay(500);
+  for (int i = 0; i < numLeds; i++) {
+    leds[i] = CRGB(0, 0, 127);
+  }
+  FastLED.show();
+  delay(500);
+  for (int i = 0; i < numLeds; i++) {
+    leds[i] = CRGB(0, 0, 0);
+  }
+  FastLED.show();
+  Serial.println("readed");
+}
+
+void onDmxFrame(uint16_t universe, uint16_t length, uint8_t sequence, uint8_t* data) {
+  sendFrame = 1;
+  // set brightness of the whole strip
+  if (universe == 15) {
+    FastLED.setBrightness(data[0]);
+    FastLED.show();
+  }
+
+  // Store which universe has got in
+  if ((universe - startUniverse) < maxUniverses) {
+    universesReceived[universe - startUniverse] = 1;
+  }
+
+  for (int i = 0; i < maxUniverses; i++) {
+    if (universesReceived[i] == 0) {
+      //Serial.println("Broke");
+      sendFrame = 0;
+      break;
+    }
+  }
+
+  // read universe and put into the right part of the display buffer
+  for (int i = 0; i < length / 3; i++) {
+    int led = i + (universe - startUniverse) * (previousDataLength / 3);
+    if (led < numLeds)
+      leds[led] = CRGB(gamma8[data[i * 3]], gamma8[data[i * 3 + 1]], gamma8[data[i * 3 + 2]]);
+  }
+  previousDataLength = length;
+
+  if (sendFrame) {
+    FastLED.show();
+    //Serial.println("Frame Now");
+    // Reset universeReceived to 0
+    memset(universesReceived, 0, maxUniverses);
+  }
+}
+
+void setup() {
+  Serial.begin(250000);
+  ConnectWifi();
+  artnet.begin();
+  FastLED.addLeds<WS2812, dataPin, GRB>(leds, numLeds);
+  initTest();
+
+  // this will be called for each packet received
+  artnet.setArtDmxCallback(onDmxFrame);
+}
+
+void loop() {
+  // we call the read function inside the loop
+  artnet.read();
+  delay(16);
+  //Serial.println("readed");
+}
