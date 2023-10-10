8a2332433fa6abc429331816d31aa570583f2b21
Antoine Pintout <antoine@ulll.xyz>
2023-10-11 00:04:58 +0200

    v1

---
+++ src/backend.h
@@ -0,0 +1,78 @@
+// OSC
+#include <WiFiUdp.h>
+#include <ArduinoOSCWiFi.h>
+
+WiFiUDP udp;
+
+IPAddress broadcastAddress;
+#define OSC_OUT_PORT 2107
+#define OSC_IN_PORT  2108
+unsigned long next_heartbeat = 0;
+
+void setupOTA(){
+    ArduinoOTA.setHostname(host);
+    ArduinoOTA.setPassword(OTA_PASS);
+    ArduinoOTA
+    .onStart([]() {
+    })
+    .onEnd([]() {
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+        digitalWrite(LED_BUILTIN, FAST_BLINK);
+    })
+    .onError([](ota_error_t error) {
+        Serial.printf("Error[%u]: ", error);
+        if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+        else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+        else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+        else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+        else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+    ArduinoOTA.begin();
+}
+
+void setupWiFi(){
+
+  long long now = millis();
+  WiFi.begin(SSID_FOULE, PSK_FOULE);
+
+  while(WiFi.status() != WL_CONNECTED && millis() < WIFI_TIMEOUT_FOULE + now){
+    vTaskDelay(LOOP_INTERVAL);
+  }
+
+  if(WiFi.status() == WL_CONNECTED){
+    delay(10);
+    broadcastAddress = (WiFi.localIP() | ~WiFi.subnetMask());
+    return;
+  }
+
+  WiFi.disconnect();
+  WiFi.begin(SSID_ENSAD, PSK_ENSAD);
+  now = millis();
+
+  while(WiFi.status() != WL_CONNECTED && millis() < WIFI_TIMEOUT_ENSAD + now){
+    vTaskDelay(LOOP_INTERVAL);
+  }
+
+  if(WiFi.status() == WL_CONNECTED){
+    delay(10);
+    broadcastAddress = (WiFi.localIP() | ~WiFi.subnetMask());
+    return;
+  }
+
+  ESP.restart();
+}
+
+void send_heartbeat(){
+  OscWiFi.send(broadcastAddress.toString(), OSC_OUT_PORT, "/heartbeat", String(host), WiFi.localIP().toString());
+}
+
+void handle_osc(){
+
+  if(millis() > next_heartbeat){
+    next_heartbeat = millis() + random(HEARTBEAT_LOW, HEARTBEAT_HIGH);
+    send_heartbeat();
+  }
+
+  OscWiFi.update();
+}
--- src/main.cpp
+++ src/main.cpp
@@ -15,131 +15,61 @@
 
 #include <Arduino.h>
 #include <Preferences.h>
-#define BAUDRATE 115200
+Preferences preferences;
 
+#define BAUDRATE 115200
+#define LOOP_INTERVAL 10  // global loop interval in ticks
 
 // Network stack
 #include <WiFi.h>
 #include <ESPmDNS.h>
 #include <ArduinoOTA.h>
-#define OTA_PASS "hugo"
+#define OTA_PASS "Bijahouix"
 
 char host[22];
 
-// OSC
-#include <WiFiUdp.h>
-#include <OSCMessage.h>
-#include <OSCBundle.h>
-#include <OSCData.h>
-WiFiUDP udp;
-
-IPAddress broadcastAddress;
-#define OSC_OUT_PORT 2107
-#define OSC_IN_PORT  2108
-unsigned long next_heartbeat = 0;
-
-
 // App
 #include <ArtnetWifi.h>
+ArtnetWiFiReceiver artnet;
+
 #define FAST_BLINK (millis() % 200 < 50)
 #define HEARTBEAT ((millis() + 1000 )% 2000 < 50)
 
-#define PWM_FREQ 1000
-#define PWM_RES_BITS 16
-#define INPUT_RES 255
-#define LOOP_INTERVAL 10  // global loop interval in ticks
-#define NUM_CHANNEL 9 
+uint16_t frame_count = 0;
 
+uint16_t univ_a = 1;
+uint16_t univ_b = 1;
+uint16_t univ_c = 1;
+
+uint16_t addr_a = 0;
+uint16_t addr_b = 0;
+uint16_t addr_c = 0;
+
+uint32_t colormult_r = 65535;
+uint32_t colormult_g = 65535;
+uint32_t colormult_b = 65535;
+
+uint32_t brightness = 32768;
+
+#include "backend.h"
+#include "led_helper.h"
 
 bool running = true;
 
-const uint16_t PROGMEM gamma28_8b_16b[] = {
-      0,    0,    0,    0,    1,    1,    2,    3,    4,    6,    8,   10,   13,   16,   19,   24,
-     28,   33,   39,   46,   53,   60,   69,   78,   88,   98,  110,  122,  135,  149,  164,  179,
-    196,  214,  232,  252,  273,  295,  317,  341,  366,  393,  420,  449,  478,  510,  542,  575,
-    610,  647,  684,  723,  764,  806,  849,  894,  940,  988, 1037, 1088, 1140, 1194, 1250, 1307,
-   1366, 1427, 1489, 1553, 1619, 1686, 1756, 1827, 1900, 1975, 2051, 2130, 2210, 2293, 2377, 2463,
-   2552, 2642, 2734, 2829, 2925, 3024, 3124, 3227, 3332, 3439, 3548, 3660, 3774, 3890, 4008, 4128,
-   4251, 4376, 4504, 4634, 4766, 4901, 5038, 5177, 5319, 5464, 5611, 5760, 5912, 6067, 6224, 6384,
-   6546, 6711, 6879, 7049, 7222, 7397, 7576, 7757, 7941, 8128, 8317, 8509, 8704, 8902, 9103, 9307,
-   9514, 9723, 9936,10151,10370,10591,10816,11043,11274,11507,11744,11984,12227,12473,12722,12975,
-  13230,13489,13751,14017,14285,14557,14833,15111,15393,15678,15967,16259,16554,16853,17155,17461,
-  17770,18083,18399,18719,19042,19369,19700,20034,20372,20713,21058,21407,21759,22115,22475,22838,
-  23206,23577,23952,24330,24713,25099,25489,25884,26282,26683,27089,27499,27913,28330,28752,29178,
-  29608,30041,30479,30921,31367,31818,32272,32730,33193,33660,34131,34606,35085,35569,36057,36549,
-  37046,37547,38052,38561,39075,39593,40116,40643,41175,41711,42251,42796,43346,43899,44458,45021,
-  45588,46161,46737,47319,47905,48495,49091,49691,50295,50905,51519,52138,52761,53390,54023,54661,
-  55303,55951,56604,57261,57923,58590,59262,59939,60621,61308,62000,62697,63399,64106,64818,65535
-};
-
 void testMode(){
+
 	for (int i = 0; i < NUM_CHANNEL; ++i)
 	{
 		ledcWrite(i, gamma28_8b_16b[(millis()/10)%200]);
 	}
 }
 
-void setupWiFi(){
-
-  long long now = millis();
-  WiFi.begin(SSID_FOULE, PSK_FOULE);
-
-  while(WiFi.status() != WL_CONNECTED or millis() < WIFI_TIMEOUT_FOULE + now){
-    vTaskDelay(LOOP_INTERVAL);
-  }
-
-  if(WiFi.status() == WL_CONNECTED){
-    return;
-    broadcastAddress = (WiFi.localIP() | ~WiFi.subnetMask());
-  }
-
-  WiFi.disconnect();
-  WiFi.begin(SSID_ENSAD, PSK_ENSAD);
-  now = millis();
-
-  while(WiFi.status() != WL_CONNECTED or millis() < WIFI_TIMEOUT_ENSAD + now){
-    vTaskDelay(LOOP_INTERVAL);
-  }
-
-  if(WiFi.status() == WL_CONNECTED){
-    return;
-    broadcastAddress = (WiFi.localIP() | ~WiFi.subnetMask());
-  }
-
-  ESP.restart();
-}
-
-void setupOTA(){
-    ArduinoOTA.setHostname(host);
-    ArduinoOTA.setPassword(OTA_PASS);
-    ArduinoOTA
-    .onStart([]() {
-    })
-    .onEnd([]() {
-    })
-    .onProgress([](unsigned int progress, unsigned int total) {
-        digitalWrite(LED_BUILTIN, FAST_BLINK);
-    })
-    .onError([](ota_error_t error) {
-        Serial.printf("Error[%u]: ", error);
-        if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-        else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-        else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-        else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-        else if (error == OTA_END_ERROR) Serial.println("End Failed");
-    });
-    ArduinoOTA.begin();
-}
-
 // front end loop
 void loop_metapixel(void * _){
 
-  preferences.begin("MPX", false);
-  int universe =   preferences.getUInt("UNIV", DEFAULT_UNIVERSE);
-  int start_addr = preferences.getUInt("SADD", DEFAULT_UNIVERSE);
 
-	ArtnetWiFiReceiver artnet;
-	artnet.begin();
+    ledcAttachPin(LED_BUILTIN, 9);
+    ledcSetup(9, PWM_FREQ, PWM_RES_BITS);
 
 	delay(10);
 
@@ -149,80 +79,129 @@ void loop_metapixel(void * _){
 		}else{
 			testMode();
 		}
-    vTaskDelay(0);
+    vTaskDelay(1);
 	}
     vTaskDelete(NULL);
 }
 
-void onDmxFrame(uint16_t universe, uint16_t length, uint8_t sequence, uint8_t* data) {
-  ledcWrite(9, gamma28_8b_16b[frame_count]);
-  frame_count ++;
-  frame_count %= 256;
-	for (int i = 0; i < NUM_CHANNEL; ++i)
-	{
-		ledcWrite(i, gamma28_8b_16b[data[i]]);
-	}
+
+void on_artnet(uint8_t pixel, const uint8_t* data, const uint16_t size){
+    uint16_t starts[3] =  {addr_a, addr_b, addr_c};
+    for (int i = 0; i < 3; ++i)
+    {
+        ledcWrite((pixel*3)+i, gamma28_8b_16b[data[starts[pixel]+i]]);
+    }
+
+    ledcWrite(9, gamma28_8b_16b[frame_count*10]);
+    frame_count ++;
+    frame_count = frame_count % 25;
 }
+
 
 void setup() {
 	pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);
-    
-	Serial.begin(BAUDRATE);
+    digitalWrite(LED_BUILTIN, HIGH);
+	pinMode(PIN_BTN, INPUT);
 
 	// build and set hostname
 	uint64_t mac = ESP.getEfuseMac();
-  uint64_t reversed_mac = 0;
+    uint64_t reversed_mac = 0;
 
-  for (int i = 0; i < 6; i++) {
-    reversed_mac |= ((mac >> (8 * i)) & 0xFF) << (8 * (5 - i));
-  }
+    for (int i = 0; i < 6; i++) {
+        reversed_mac |= ((mac >> (8 * i)) & 0xFF) << (8 * (5 - i));
+    }
 
-  snprintf(host, 22, "MPX-%llX", reversed_mac);
-  Serial.println(host);
+    snprintf(host, 22, "MPX-%llX", reversed_mac);
 
-  setupWiFi();
-  setupOTA();
-  udp.begin(OSC_IN_PORT);
+    setupWiFi();
+    setupOTA();
+    setupLED();
 
-	ledcAttachPin(PIN_A_R, 0);
-	ledcAttachPin(PIN_A_G, 1);
-	ledcAttachPin(PIN_A_B, 2);
-	ledcAttachPin(PIN_B_R, 3);
-	ledcAttachPin(PIN_B_G, 4);
-	ledcAttachPin(PIN_B_B, 5);
-	ledcAttachPin(PIN_C_R, 6);
-	ledcAttachPin(PIN_C_G, 7);
-	ledcAttachPin(PIN_C_B, 8);
-	ledcAttachPin(LED_BUILTIN, 9);
-	pinMode(PIN_BTN, INPUT);
+    univ_a = preferences.getUInt("univ_a", univ_a);
+    univ_b = preferences.getUInt("univ_b", univ_b);
+    univ_c = preferences.getUInt("univ_c", univ_c);
 
-	ledcSetup(0, PWM_FREQ, PWM_RES_BITS);
-	ledcSetup(1, PWM_FREQ, PWM_RES_BITS);
-	ledcSetup(2, PWM_FREQ, PWM_RES_BITS);
-	ledcSetup(3, PWM_FREQ, PWM_RES_BITS);
-	ledcSetup(4, PWM_FREQ, PWM_RES_BITS);
-	ledcSetup(5, PWM_FREQ, PWM_RES_BITS);
-	ledcSetup(6, PWM_FREQ, PWM_RES_BITS);
-	ledcSetup(7, PWM_FREQ, PWM_RES_BITS);
-	ledcSetup(8, PWM_FREQ, PWM_RES_BITS);
-	ledcSetup(9, PWM_FREQ, PWM_RES_BITS);
+    addr_a = preferences.getUInt("addr_a", addr_a);
+    addr_b = preferences.getUInt("addr_b", addr_b);
+    addr_c = preferences.getUInt("addr_c", addr_c);
 
-  xTaskCreatePinnedToCore(
-    loop_metapixel,     // Function that should be called
-    "Metapixel",    // Name of the task (for debugging)
-    20000,           // Stack size (bytes)
-    NULL,            // Parameter to pass
-    1,               // Task priority
-    NULL,            // Task handle
-    1                // pin to core 1
-  );
-  delay(10);
-  
-  digitalWrite(LED_BUILTIN, LOW);
+    colormult_r = (uint32_t)preferences.getUInt("colormult_r", colormult_r);
+    colormult_g = (uint32_t)preferences.getUInt("colormult_g", colormult_g);
+    colormult_b = (uint32_t)preferences.getUInt("colormult_b", colormult_b);
+
+    brightness = (uint32_t)preferences.getUInt("brightness", brightness);
+
+    artnet.begin();
+
+    artnet.subscribe(univ_a,
+        [&](const uint8_t* data, const uint16_t size) {on_artnet(0, data, size);});
+    artnet.subscribe(univ_b,
+        [&](const uint8_t* data, const uint16_t size) {on_artnet(1, data, size);});
+    artnet.subscribe(univ_c,
+        [&](const uint8_t* data, const uint16_t size) {on_artnet(2, data, size);});
+
+    OscWiFi.subscribe(OSC_IN_PORT, "/addresses", addr_a, addr_b, addr_c);
+    OscWiFi.subscribe(OSC_IN_PORT, "/calibration", colormult_r, colormult_g, colormult_b);
+    OscWiFi.subscribe(OSC_IN_PORT, "/brightness", brightness);
+    
+    OscWiFi.subscribe(OSC_IN_PORT, "/universes",
+        [&](
+            const int& _univ_a,
+            const int& _univ_b,
+            const int& _univ_c
+            ) {
+
+            univ_a = _univ_a;
+            univ_b = _univ_b;
+            univ_c = _univ_c;
+
+            artnet.unsubscribe();
+
+            artnet.subscribe(univ_a,
+                [&](const uint8_t* data, const uint16_t size) {on_artnet(0, data, size);});
+            artnet.subscribe(univ_b,
+                [&](const uint8_t* data, const uint16_t size) {on_artnet(1, data, size);});
+            artnet.subscribe(univ_c,
+                [&](const uint8_t* data, const uint16_t size) {on_artnet(2, data, size);});
+
+        });
+
+    OscWiFi.subscribe(OSC_IN_PORT, "/save", [](){
+        preferences.putUInt("univ_a", univ_a);
+        preferences.putUInt("univ_b", univ_b);
+        preferences.putUInt("univ_c", univ_c);
+
+        preferences.putUInt("addr_a", addr_a);
+        preferences.putUInt("addr_b", addr_b);
+        preferences.putUInt("addr_c", addr_c);
+
+        preferences.putUInt("colormult_r", (uint16_t)colormult_r);
+        preferences.putUInt("colormult_g", (uint16_t)colormult_g);
+        preferences.putUInt("colormult_b", (uint16_t)colormult_b);
+
+        preferences.putUInt("brightness", (uint16_t)brightness);
+    });
+
+    xTaskCreatePinnedToCore(
+        loop_metapixel,     // Function that should be called
+        "Metapixel",    // Name of the task (for debugging)
+        20000,           // Stack size (bytes)
+        NULL,            // Parameter to pass
+        1,               // Task priority
+        NULL,            // Task handle
+        !xPortGetCoreID()// pin to not the one running this
+    );
+
+    delay(10);
+
+    digitalWrite(LED_BUILTIN, LOW);
+
+    delay(100);
+    Serial.begin(BAUDRATE);
+    delay(400);
+    Serial.println(host);
+    Serial.println(broadcastAddress);
 }
-
-int frame_count;
 
 // backend loop
 void loop(){
@@ -239,43 +218,4 @@ void loop(){
   // OTA
   ArduinoOTA.handle();
   vTaskDelay(LOOP_INTERVAL);
-}
-
-void send_heartbeat(){
-  OSCMessage msg("/heartbeat");
-  msg.add(host);
-  udp.beginPacket(broadcastAddress, OSC_OUT_PORT);
-  msg.send(osc_out_socket);
-  udp.endPacket();
-  msg.empty();
-}
-
-void handle_osc(){
-
-  if(millis() > next_heartbeat){
-    next_heartbeat = millis() + random(HEARTBEAT_LOW, HEARTBEAT_HIGH);
-    send_heartbeat();
-  }
-
-
-  OSCMessage message;
-  int size = udp.parsePacket();
-
-  if (size > 0) {
-    while (size--) {
-      message.fill(udp.read());
-    }
-    if (!message.hasError()) {
-      message.dispatch("/update_addr", update_addr);
-      message.dispatch("/update_univ", update_univ);
-      /*message.dispatch("/color_r", update_color_red);
-      message.dispatch("/color_g", update_color_green);
-      message.dispatch("/color_b", update_color_blue);
-      message.dispatch("/brightness",  update_bright);
-      message.dispatch("/restart",     osc_restart);
-      message.dispatch("/blackout",    osc_blackout);
-      message.dispatch("/whiteout",    osc_whiteout);
-      message.dispatch("/artnetout",   osc_arnetout);*/
-    }
-  }
 }
